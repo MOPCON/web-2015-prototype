2c0c8380ee774b3543667647e7752461b697588f
Wcc723 <agito723@gmail.com>
2015-06-05 08:23:01 +0800

    fixed flickr api path

--- source/javascripts/jsx/mopcon.jsx
+++ source/javascripts/jsx/mopcon.jsx
@@ -31,7 +31,8 @@ var MopconIntro = React.createClass({
 	componentDidMount: function() {
 		photos = [];
 		// api link https://www.flickr.com/services/api/explore/flickr.groups.pools.getPhotos
-		var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=e8a0be0224faddd8b45f1170c0e555eb&group_id=2768675%40N23&per_page=500&page=2&format=json&nojsoncallback=1";
+		// var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=6534b52dffe2164a73b1388299b36461&group_id=2768675%40N23&per_page=500&page=2&format=json&api_sig=07241605a5653d9e4ac18c4344722722";
+		var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=6534b52dffe2164a73b1388299b36461&group_id=2768675%40N23&per_page=500&page=2&format=json&nojsoncallback=1&api_sig=1253d9df799c3d5d7fe6fcd580ef0236";
 		$.getJSON( flickerAPI, {
 			format: "json"
 		}).done(function( data ) {
