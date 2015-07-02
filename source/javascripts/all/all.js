3f84c271026a7f582c9df79f800bc4e8f5a5c745
ilandy <ilandy@infbeyond.com>
2015-07-02 17:27:04 +0800

    調整整體顏色與呈現

--- source/javascripts/all/all.js
+++ source/javascripts/all/all.js
@@ -1,2 +1,10 @@
 //= require jquery
 //= require_tree .
+$(document).ready(function(){
+	$('.bg-gray').hover(function(){
+		$(this).addClass('bg-coloranimation');
+	},function(){
+		$(this).removeClass('bg-coloranimation');
+	});
+		
+});
