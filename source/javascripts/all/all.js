4e83870da161637b06d8fd09583b7bb88883f084
ilandy <ilandy@infbeyond.com>
2015-09-19 13:15:50 +0800

    1. 修改 pdreset 2. 修改 無法清楚呈現所選日期的議程

--- source/javascripts/all/all.js
+++ source/javascripts/all/all.js
@@ -6,14 +6,22 @@ $(document).ready(function(){
 	},function(){
 		$(this).removeClass('bg-coloranimation');
 	});
-	$('.slideDown').on('click',function(){
+	$('.i-slideDown').on('click',function(){
 		$(this).toggleClass('active').parents('.schedule-body').next('.schedule-footer').toggleClass('active');
 	});
 	$('.mbl-date').on('click',function(){
 		$('[data-schedule]').addClass('hidden-xs').eq($(this).index()).toggleClass('hidden-xs');
+		$('.getSchdule div').toggleClass('active');
 	});
 	$('.mbl-date').on('click',function(){
 		$('.mbl-date').toggleClass('active');
 	});
+	$(document).scroll(function (event) {
+        if ($(document).scrollTop() > 350) {
+            $(".getSchdule").addClass('active');
+        } else {
+            $(".getSchdule").removeClass('active');
+        }
+    });
 		
 });
